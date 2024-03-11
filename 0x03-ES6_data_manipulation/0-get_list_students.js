8962d703921403a669fd9ed6d87ca48b31b9d212
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-03-11 15:39:54 +0300

    Added nodejs modules

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+function getListStudents() {
+    const objects = [
+        { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
+        { id: 2, firstname: 'James', location: 'Columbia' },
+        { id: 3, firstname: 'Serena', location: 'San Francisco' }
+    ];
+
+    return objects;
+}
