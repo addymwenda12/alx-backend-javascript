3b466b692690a171cebd08c2122a25f46052e4e1
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-03-12 22:01:00 +0300

    returns the sum of all the student ids

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,10 @@
+import getListStudents from "./0-get_list_students.js";
+
+function getStudentIdsSum(students) {
+    return students.reduce((sum, student) => sum + student.id, 0);
+}
+
+const students = getListStudents();
+const value = getStudentIdsSum(students);
+
+console.log(value);
