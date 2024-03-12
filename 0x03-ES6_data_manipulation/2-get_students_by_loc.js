48bd3f8c5790c0997c6c9431aa59764d2e0e6a6a
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-03-12 21:58:09 +0300

    returns an array of objects who are located in a specific city

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,7 @@
+import getListStudents from "./0-get_list_students.js";
+
+function getStudentsByLocation(students, city) {
+    return students.filter(student => student.location === city);
+}
+
+export default getStudentsByLocation;
