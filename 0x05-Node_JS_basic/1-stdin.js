79962d2478970167e658eaf16caa1d6a4f56835d
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-04-29 14:38:37 +0300

    Added readline interface that reads user input and displays a message

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,17 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+console.log('Welcome to Holberton School, what is your name?');
+
+rl.question('', (answer) => {
+    console.log(`Your name is: ${answer}`);
+    rl.close();
+});
+
+rl.on('close', () => {
+    console.log('This important software is now closing');
+});
