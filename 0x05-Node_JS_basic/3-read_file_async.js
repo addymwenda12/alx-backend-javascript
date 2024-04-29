77c06672edfcd68bcb0cf16eeb6aa6e647f9fd9b
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-04-29 18:07:33 +0300

    Implemented asynchronous CSV pairing in countStudents function

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,36 @@
+const fs = require('fs').promises;
+
+function countStudents(path) {
+    return fs.readFile(path, 'utf-8') 
+    .then((data) => {
+        const lines = data.split('\n');
+        const nonEmptyLines = lines.filter(line => line.trim() !== "");
+
+        const fieldsCount = {};
+
+        for (let i = 1; i < nonEmptyLines.length; i++) {
+            const line = nonEmptyLines[i];
+            const [firstName, , , field] = line.split(',');
+            if (!fieldsCount[field]) {
+                fieldsCount[field] = {
+                    count: 0,
+                    students: []
+                };
+            }
+            fieldsCount[field].count++;
+            fieldsCount[field].students.push(firstName.trim());
+        }
+
+        const totalStudents = nonEmptyLines.length - 1;
+        console.log(`Number of students: ${totalStudents}`);
+
+        for (const field in fieldsCount) {
+            console.log(`Number of students in ${field}: ${fieldsCount[field].count}. List: ${fieldsCount[field].students.join(', ')}`);
+        }
+    })
+    .catch(() => {
+        throw new Error('Cannot load the database');
+    });
+}
+
+module.exports = countStudents;
