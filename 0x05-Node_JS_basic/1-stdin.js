93d75c09d6955885f0388d659da1c7e5dffdc90c
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-04-30 14:18:25 +0300

    Implemented interactive name input and response

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,7 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
-    process.exit();
-});
-
-process.stdin.on('end', () => {
+    process.stdout.write(`Your name is: ${data}`);
     process.stdout.write('This important software is now closing\n');
+    process.exit();
 });
