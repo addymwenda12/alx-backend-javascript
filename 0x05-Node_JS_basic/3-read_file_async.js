48ebb60af1072fd6ed56cb33c270b39e78c4547c
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-05-03 22:11:10 +0300

    Fixed linting errors

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,35 +1,35 @@
 const fs = require('fs').promises;
 
 function countStudents(path) {
-    return fs.readFile(path, 'utf-8') 
+  return fs.readFile(path, 'utf-8')
     .then((data) => {
-        const lines = data.split('\n');
-        const nonEmptyLines = lines.filter(line => line.trim() !== "");
+      const lines = data.split('\n');
+      const nonEmptyLines = lines.filter((line) => line.trim() !== '');
 
-        const fieldsCount = {};
+      const fieldsCount = {};
 
-        for (let i = 1; i < nonEmptyLines.length; i++) {
-            const line = nonEmptyLines[i];
-            const [firstName, , , field] = line.split(',');
-            if (!fieldsCount[field]) {
-                fieldsCount[field] = {
-                    count: 0,
-                    students: []
-                };
-            }
-            fieldsCount[field].count++;
-            fieldsCount[field].students.push(firstName.trim());
+      for (let i = 1; i < nonEmptyLines.length; i += 1) {
+        const line = nonEmptyLines[i];
+        const [firstName, , , field] = line.split(',');
+        if (!fieldsCount[field]) {
+          fieldsCount[field] = {
+            count: 0,
+            students: [],
+          };
         }
+        fieldsCount[field].count += 1;
+        fieldsCount[field].students.push(firstName.trim());
+      }
 
-        const totalStudents = nonEmptyLines.length - 1;
-        console.log(`Number of students: ${totalStudents}`);
+      const totalStudents = nonEmptyLines.length - 1;
+      console.log(`Number of students: ${totalStudents}`);
 
-        for (const field in fieldsCount) {
-            console.log(`Number of students in ${field}: ${fieldsCount[field].count}. List: ${fieldsCount[field].students.join(', ')}`);
-        }
+      for (const field in fieldsCount) {
+        console.log(`Number of students in ${field}: ${fieldsCount[field].count}. List: ${fieldsCount[field].students.join(', ')}`);
+      }
     })
     .catch(() => {
-        throw new Error('Cannot load the database');
+      throw new Error('Cannot load the database');
     });
 }
 
