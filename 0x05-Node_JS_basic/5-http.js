41e47b32c89c2b99b6392054695eafba68959fe2
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-05-04 16:17:21 +0300

    Improved student data processing and add prototype chain check in for-in loop

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,34 +1,55 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
-
-const databasePath = process.argv[2];
+const fs = require('fs');
 
 const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  if (req.url === '/') {
-    res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
-    countStudents(databasePath)
-      .then((data) => {
-        let response = 'This is the list of our students\n';
-        response += `Number of students: ${data.length}\n`;
-        data.forEach((field) => {
-          response += `Number of students in ${field.field}: `;
-          response += `${field.students.length}. List: ${field.students.join(', ')}\n`;
+  const { url } = req;
+  const databaseFile = process.argv[2];
+
+  if (url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!\n');
+  } else if (url === '/students') {
+    fs.readFile(databaseFile, 'utf8', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+        return;
+      }
+
+      const students = data.trim().split('\n').filter((line) => line.trim() !== '');
+      const getStudentInfo = (students) => {
+        const counts = { CS: 0, SWE: 0 };
+        const groups = { CS: [], SWE: [] };
+
+        students.forEach((student) => {
+          const [name, field] = student.split(',');
+          if (field === 'CS' || field === 'SWE') {
+            counts[field] += 1;
+            groups[field].push(name);
+          }
         });
-        res.end(response);
-      })
-      .catch((error) => {
-        res.statusCode = 500;
-        res.end(error.message);
-      });
+
+        let info = `Number of students: ${students.length}\n`;
+        for (const field in counts) {
+          if (Object.prototype.hasOwnProperty.call(counts, field)) {
+            info += `Number of students in ${field}: ${counts[field]}. List: ${groups[field].join(', ')}\n`;
+          }
+        }
+        return info;
+      };
+
+      const studentInfo = getStudentInfo(students);
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end(`This is the list of our students\n${studentInfo}`);
+    });
   } else {
-    res.statusCode = 404;
-    res.end('Page not found');
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found\n');
   }
 });
 
-app.listen(1245);
+app.listen(1245, () => {
+  console.log('Server is running on http://localhost:1245');
+});
 
 module.exports = app;
