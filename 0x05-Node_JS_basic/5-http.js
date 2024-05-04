3d679e51ada34c7a015e008bf394dd5e171bf859
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-05-04 16:01:08 +0300

    Refactored HTTP server to handle errors and improve response formatting

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -4,22 +4,29 @@ const countStudents = require('./3-read_file_async');
 const databasePath = process.argv[2];
 
 const app = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    if (req.url === '/') {
-        res.end('Hello Holberton School!');
-    } else if (req.url === '/students') {
-        countStudents(databasePath)
-            .then((data) => {
-                res.write('This is the list of our students\n');
-                res.end(data);
-            })
-            .catch((error) => {
-                res.end(error.message);
-            });
-    } else {
-        res.end('Page not found');
-    }
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    countStudents(databasePath)
+      .then((data) => {
+        let response = 'This is the list of our students\n';
+        response += `Number of students: ${data.length}\n`;
+        data.forEach((field) => {
+          response += `Number of students in ${field.field}: `;
+          response += `${field.students.length}. List: ${field.students.join(', ')}\n`;
+        });
+        res.end(response);
+      })
+      .catch((error) => {
+        res.statusCode = 500;
+        res.end(error.message);
+      });
+  } else {
+    res.statusCode = 404;
+    res.end('Page not found');
+  }
 });
 
 app.listen(1245);
